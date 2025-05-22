bc518f62c1ce1b7d182e9ff2587e245206bb05a8
Rejka Permana <rejkamuhammad@gmail.com>
2025-05-22 13:20:39 +0700

    update navbar and auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "home/index"
+  root "home#index"
+  get "registrations/new"
+  resource :session
+  resource :registration, only: %i[new create]
+  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
