9cfb4b27b7389b4d32829c3afdd8f3172efdd34c
Rejka Permana <rejkamuhammad@gmail.com>
2025-05-22 13:43:47 +0700

    update sidebar and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+  include SvgIconsHelper
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :sidebar_menu
+
+  def sidebar_menu
+    @sidebar_menus = [
+      { name: "Dashboard", path: "/", icon: dashboard },
+      { name: "Recipes", path: "/recipes", icon: recipes },
+      { name: "Ingredients", path: "/ingredients", icon: ingredients }
+    ]
+  end
 end
